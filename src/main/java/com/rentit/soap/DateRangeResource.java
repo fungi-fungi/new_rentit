7076fcf4729ee53d3a3dde8a7d66a17634c27314
psyfungus <psyfungus@gmai.com>
2013-10-31 01:34:14 +0200

    one more fix

---
+++ src/main/java/com/rentit/soap/DateRangeResource.java
@@ -0,0 +1,14 @@
+package com.rentit.soap;
+
+import java.util.Date;
+
+import org.springframework.roo.addon.javabean.RooJavaBean;
+
+@RooJavaBean
+public class DateRangeResource {
+	
+	Date start;
+	
+	Date end;
+
+}
