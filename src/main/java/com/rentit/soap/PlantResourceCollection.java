e64a5c4ff02aa4ec604d3b4ef2e53e367f2d9826
Kaupo <kuresson@gmail.com>
2013-10-31 19:19:58 +0200

    names

--- src/main/java/com/rentit/soap/PlantResourceCollection.java
+++ src/main/java/com/rentit/soap/PlantResourceCollection.java
@@ -7,12 +7,12 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 import org.springframework.roo.addon.javabean.RooJavaBean;
 
-@XmlRootElement(name = "plants")
+@XmlRootElement
 @RooJavaBean
 public class PlantResourceCollection {
 
 	// To change representation of object in XML you need to annotait getter.
-	@XmlElement
+	@XmlElement(name = "plants")
 	private List<PlantResource> listOfPlants;
 
 	public void addPlant(PlantResource resource) {
