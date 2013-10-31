6076635fea9d312fb8dc011de8fd4df981729381
Kaupo <kuresson@gmail.com>
2013-10-31 19:11:11 +0200

    Testing

--- src/main/java/com/rentit/soap/PlantResourceCollection.java
+++ src/main/java/com/rentit/soap/PlantResourceCollection.java
@@ -14,19 +14,9 @@ public class PlantResourceCollection {
 	// To change representation of object in XML you need to annotait getter.
 	private List<PlantResource> listOfPlants;
 
-	@XmlElement(name = "plant")
-	public List<PlantResource> getListOfPlants() {
-		return listOfPlants;
-	}
-
-	public void setListOfPlants(List<PlantResource> listOfPlants) {
-		this.listOfPlants = listOfPlants;
-	}
-
-	public void addPlant(PlantResource plant) {
+	public void addPlant(PlantResource resource) {
 		if(listOfPlants == null) {
 			listOfPlants = new ArrayList<PlantResource>();
-		}
-		this.listOfPlants.add(plant);
+		}		
 	}
 }
